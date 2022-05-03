e57ad713a4f8d70eba5817c96cda170cec2895d3
huobingli <huobingli@outlook.com>
2022-05-04 03:51:23 +0800

    update

--- dataView/src/components/TaskTable.vue
+++ dataView/src/components/TaskTable.vue
@@ -15,12 +15,13 @@
           :width="header.width"
         >
         </el-table-column>
-        <el-table-column fixed="right" label="操作" width="100">
+        <el-table-column label="操作" width="150">
           <template slot-scope="scope">
             <el-button @click="handleClick(scope.row)" type="text" size="small"
               >查看</el-button
             >
             <el-button type="text" size="small">编辑</el-button>
+            <el-button type="text" size="small">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -92,6 +93,9 @@ export default {
     handleClearTableList: function () {
       this.tableData = null;
     },
+    handleClick: function(data) {
+      console.log(data)
+    }
   },
 };
 </script>
